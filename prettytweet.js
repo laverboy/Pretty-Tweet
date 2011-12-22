490c8bf2137fd1cb7d409efef50976b288233fb2
Matthew Laver <matthewlaver@gmail.com>
2011-12-22 15:54:18 +0000

    Update prettytweet.js

--- prettytweet.js
+++ prettytweet.js
@@ -48,14 +48,14 @@ jQuery.fn.getTweet = function(user){
 	});
 	request.done(function(data){
 		// result returned
-        var tweet = data[0].text;
+        	var tweet = data[0].text;
       
-        // process links and reply
-        tweet = tweet.replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig, function(url) {
-            return '<a href="'+url+'">'+url+'</a>';
-        }).replace(/B@([_a-z0-9]+)/ig, function(reply) {
-            return  reply.charAt(0)+'<a href="http://twitter.com/'+reply.substring(1)+'">'+reply.substring(1)+'</a>';
-        });
+	        // process links and reply
+	        tweet = tweet.replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig, function(url) {
+	            return '<a href="'+url+'">'+url+'</a>';
+	        }).replace(/B@([_a-z0-9]+)/ig, function(reply) {
+	            return  reply.charAt(0)+'<a href="http://twitter.com/'+reply.substring(1)+'">'+reply.substring(1)+'</a>';
+	        });
 
 		var fulldate = data[0].created_at,
 		date = prettyDate(fulldate),
